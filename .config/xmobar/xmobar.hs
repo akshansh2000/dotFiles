711aca68fb14004f241c8ed94561ecd42bb3fcf9
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-08-04 15:31:07 +0530

    full fledged xmobar now

--- .config/xmobar/xmobar.hs
+++ .config/xmobar/xmobar.hs
@@ -1,18 +1,18 @@
 Config { 
 
    -- appearance
-     font =         "xft:Mononoki:size=10:bold:antialias=true"
-   , bgColor =      "#000000"
-   , alpha =        100
+     font =         "xft:Mononoki:size=10:bold:antialiasing=true"
+   , bgColor =      "#0d0d0d"
+   , alpha =        255
    , fgColor =      "#646464"
    , position =     Top
    , border =       NoBorder
-   , borderColor =  "#000000"
+   , borderColor =  "#2a2a2a"
 
    -- layout
    , sepChar =  "%"   -- delineator between plugin names and straight text
    , alignSep = "}{"  -- separator between left-right alignment
-   , template = "%battery% | %multicpu% | %coretemp% | %memory% | %dynnetwork% }{ %date% || %kbd% "
+   , template = "%battery% | %multicpu% | %coretemp% | %memory% | %swap% } %StdinReader% { %enp59s0% %wlan0wi% | %default:Master%| %bright% | %date% || %kbd% "
 
    -- general behavior
    , lowerOnStart =     True    -- send to bottom of window stack on start
@@ -51,6 +51,9 @@ Config {
                              , "--high"     , "darkred"
                              ] 10
 
+	-- StdinReader
+	, Run StdinReader
+
         -- cpu activity monitor
         , Run MultiCpu       [ "--template" , "Cpu: <total0>%|<total1>%|<total2>%|<total3>%"
                              , "--Low"      , "30"         -- units: %
@@ -70,13 +73,16 @@ Config {
                              ] 50
                           
         -- memory usage monitor
-        , Run Memory         [ "--template" ,"Mem: <usedratio>%"
+        , Run Memory         [ "--template" , "Mem: <usedratio>%"
                              , "--Low"      , "50"        -- units: %
                              , "--High"     , "80"        -- units: %
                              , "--low"      , "darkgreen"
                              , "--normal"   , "darkorange"
                              , "--high"     , "darkred"
                              ] 10
+	
+	-- swap usage monitor
+	, Run Swap 	     [] 10
 
 	-- memory used (MB)
 	-- , Run Memory         [ "--template" ,"Mem: <used>M"
@@ -95,6 +101,23 @@ Config {
         --                      , "--normal"   , "darkorange"
         --                      , "--high"     , "darkgreen"
         --                      ] 10
+
+	-- volume
+	, Run Volume "default" "Master" [] 1
+
+	-- brightness
+	, Run Brightness	[ "--template", "Bri: <percent>"
+				, "--"
+				, "-D"	      , "intel_backlight"
+				] 1
+
+	-- ethernet
+	, Run Network "enp59s0" [ "--template", "LAN: <fc=#08B408><dev></fc> |"
+			        ] 10
+
+	-- wifi
+	, Run Wireless "wlan0" [ "--template", "WiFi: <essid> (<quality>%)"
+			       ] 10
 
         -- battery monitor
         , Run Battery        [ "--template" , "Batt: <acstatus>"
