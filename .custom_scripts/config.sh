6446c615893c12aa2964d194003b8be46519fee5
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-10-16 06:31:44 +0530

    new .sh files don't open on custom rofi input

--- .custom_scripts/config.sh
+++ .custom_scripts/config.sh
@@ -1,34 +1,36 @@
+#!/bin/bash
+
 option=$(echo -e "nvim\nzsh\nxresources\ni3\nsxhkd\ncustom scripts\npolybar\ndunst\ni3lock\nrofi\nbash\nxinit\nxmonad\nxmobar\nalacritty" | rofi -show drun -i -dmenu --no-custom -p "config")
 
 term=$(which urxvt)
 
-if [ $option == "zsh" ]; then
+if [ "$option" == "zsh" ]; then
   $term -e nvim ~/.zshrc
-elif [ $option == "i3" ]; then
+elif [ "$option" == "i3" ]; then
   $term -e nvim ~/.config/i3/config
-elif [ $option == "sxhkd" ]; then
+elif [ "$option" == "sxhkd" ]; then
   $term -e nvim ~/.config/sxhkd/sxhkdrc 
-elif [ $option == "dunst" ]; then
+elif [ "$option" == "dunst" ]; then
   $term -e nvim ~/.config/dunst/dunstrc
-elif [ $option == "i3lock" ]; then
+elif [ "$option" == "i3lock" ]; then
   $term -e nvim ~/.config/i3lock/.i3lockrc
-elif [ $option == "bash" ]; then
+elif [ "$option" == "bash" ]; then
   $term -e nvim ~/.bashrc
-elif [ $option == "xinit" ]; then
+elif [ "$option" == "xinit" ]; then
   $term -e nvim ~/.xinitrc
-elif [ $option == "xmonad" ]; then
+elif [ "$option" == "xmonad" ]; then
   $term -e nvim ~/.xmonad/xmonad.hs
-elif [ $option == "xmobar" ]; then
+elif [ "$option" == "xmobar" ]; then
   $term -e nvim ~/.config/xmobar/xmobar.hs
-elif [ $option == "alacritty" ]; then
+elif [ "$option" == "alacritty" ]; then
   $term -e nvim ~/.config/alacritty/alacritty.yml
-elif [ $option == "polybar" ]; then
+elif [ "$option" == "polybar" ]; then
   $term -e nvim ~/.config/polybar/config
-elif [ $option == "nvim" ]; then
+elif [ "$option" == "nvim" ]; then
   $term -e nvim ~/.config/nvim/init.vim
-elif [ $option == "xresources" ]; then
+elif [ "$option" == "xresources" ]; then
   $term -e nvim ~/.Xresources
-elif [ $option == "rofi" ]; then
+elif [ "$option" == "rofi" ]; then
   $term -e nvim ~/.config/rofi/config.rasi
 elif [ "$option" == "custom scripts" ]; then
   ~/.custom_scripts/scripts_config.sh
--- .custom_scripts/scripts_config.sh
+++ .custom_scripts/scripts_config.sh
@@ -1,3 +1,7 @@
-option=$(echo -e "battery\nconfig\ncopy_config\nexit\nmonitors\npolybar\ntemperature\nxinput" | rofi -i -dmenu -no-custom -p "custom scripts")
+#!/bin/bash
 
-urxvt -e nvim ~/.custom_scripts/$option.sh
+option=$(echo -e "battery\nconfig\ncopy_config\nexit\nmonitors\npolybar\nscripts_config\ntemperature\nxinput" | rofi -i -dmenu -no-custom -p "custom scripts")
+
+if [ -f ~/.custom_scripts/$option.sh ]; then
+  urxvt -e nvim ~/.custom_scripts/$option.sh
+fi
