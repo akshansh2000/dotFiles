9c4c293abc731620dcb62de20488522e5ff4c54e
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-10-30 02:50:12 +0530

    removed useless echo

--- .custom_scripts/emojis.sh
+++ .custom_scripts/emojis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-emoji=$(echo -e "$(cat ~/.custom_scripts/emojis)" | rofi -i -dmenu --no-custom -p "emoji")
+emoji=$(cat ~/.custom_scripts/emojis | rofi -i -dmenu --no-custom -p "emoji")
 echo $emoji | cut -d' ' -f1 | xclip -selection clipboard
