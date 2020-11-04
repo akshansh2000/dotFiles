442fe537120fbae83afc88e074f1f2153a5669d7
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-11-04 19:12:45 +0530

    minor typos

--- .custom_scripts/emojis.sh
+++ .custom_scripts/emojis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-emoji=$(cat ~/.custom_scripts/emojis | rofi -i -dmenu --no-custom -p "emoji")
+emoji=$(cat ~/.custom_scripts/emojis | rofi -i -dmenu -no-custom -p "emojis")
 echo $emoji | cut -d' ' -f1 | xclip -selection clipboard
--- .custom_scripts/scripts_config.sh
+++ .custom_scripts/scripts_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-option=$(echo -e "battery\nconfig\ncopy_config\nexit\nmonitors\npolybar\nscripts_config\ntemperature\nxinput" | rofi -i -dmenu -no-custom -p "custom scripts")
+option=$(echo -e "battery\nconfig\ncopy_config\nemojis\nexit\nmonitors\npolybar\nscripts_config\ntemperature\nxinput" | rofi -i -dmenu -no-custom -p "custom scripts")
 
 if [ -f ~/.custom_scripts/$option.sh ]; then
   urxvt -e nvim ~/.custom_scripts/$option.sh
