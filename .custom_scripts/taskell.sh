dcabf7b7bae360c00d21121c322ee6c41f9cd4d1
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-12-27 00:08:21 +0530

    another taskell list, reduced rofi size

--- .custom_scripts/taskell.sh
+++ .custom_scripts/taskell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-option=$(echo -e "toDo\ntoWatch" | rofi -show drun -i -dmenu -no-custom -p "taskell")
-urxvt -e taskell ~/Documents/taskell/$option.md
+option=$(echo -e "toDo\ntoWatch\nflags" | rofi -show drun -i -dmenu -no-custom -p "taskell")
+[ "$option" = "" ] || urxvt -e taskell ~/Documents/taskell/$option.md
