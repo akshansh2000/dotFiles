39a77d4a9d8db5287f19158fc4ab219f6d7803ed
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-12-28 22:58:24 +0530

    rofi size, polybar workspace scrolling, taskell dynamic files

--- .custom_scripts/taskell.sh
+++ .custom_scripts/taskell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-option=$(echo -e "toDo\ntoWatch\nflags" | rofi -show drun -i -dmenu -no-custom -p "taskell")
+option=$(for file in ~/Documents/taskell/*; do echo $(basename $file) | cut -d. -f1; done | rofi -show drun -i -dmenu -no-custom -p "taskell")
 [ "$option" = "" ] || urxvt -e taskell ~/Documents/taskell/$option.md
