72dbc536b0d92432dfe0ccdf42bf72f929f3da15
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-12-30 02:44:57 +0530

    black default, polybar added empty bottom, added wallpapers

--- .custom_scripts/polybar.sh
+++ .custom_scripts/polybar.sh
@@ -2,8 +2,10 @@ killall polybar 2> /dev/null
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c /home/akshansh2000/.config/polybar/config --reload example &
+    MONITOR=$m polybar -c /home/akshansh2000/.config/polybar/config --reload top &
+    MONITOR=$m polybar -c /home/akshansh2000/.config/polybar/config --reload bottom &
   done
 else
-  polybar -c /home/akshansh2000/.config/polybar/config --reload example
+  polybar -c /home/akshansh2000/.config/polybar/config --reload top &
+  polybar -c /home/akshansh2000/.config/polybar/config --reload bottom &
 fi
