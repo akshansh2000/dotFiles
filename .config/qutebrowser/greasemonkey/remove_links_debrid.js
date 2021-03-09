8e9da5e6b5b3281f5783fbb7325d3e1018686935
Akshansh Bhanjana <akshansh2000@gmail.com>
2021-03-10 01:24:15 +0530

    added debrid link removal greasemonkey script

---
+++ .config/qutebrowser/greasemonkey/remove_links_debrid.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Remove Locked Links - Real Debrid
+// @namespace    https://akshansh2000.github.io
+// @version      0.1
+// @description  remove locked links list on Real-Debrid's torrents page
+// @author       akshansh2000
+// @match        https://real-debrid.com/torrents
+// @grant        none
+// ==/UserScript==
+
+let elements = document.getElementsByTagName("textarea");
+for (let element of elements)
+  element.setAttribute("rows", "1");
