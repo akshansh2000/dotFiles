a18ec3baef2a0eb5902d3e56c7f8a8ca03f27d37
Akshansh Bhanjana <akshansh2000@gmail.com>
2021-04-11 14:24:14 +0530

    new todo lists, reload picom, bookmarks

--- .custom_scripts/reload.sh
+++ .custom_scripts/reload.sh
@@ -4,3 +4,4 @@ i3-msg "restart"
 killall picom 2> /dev/null
 sleep 0.2
 picom &
+~/.custom_scripts/polybar.sh
--- .custom_scripts/taskell.sh
+++ .custom_scripts/taskell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 option=$(for file in ~/Documents/taskell/*; do echo $(basename $file) | cut -d. -f1; done | rofi -show drun -i -dmenu -no-custom -p "taskell")
-[ "$option" = "" ] || urxvt -e taskell ~/Documents/taskell/$option.md
+[ "$option" = "" ] || alacritty -e taskell ~/Documents/taskell/$option.md
