cc63365ce34317a5589f8060d205292ad9e1c6f8
Akshansh Bhanjana <akshansh2000@gmail.com>
2021-04-19 22:41:40 +0530

    changed default editor, brightness changes on lock screen

--- .custom_scripts/config.sh
+++ .custom_scripts/config.sh
@@ -2,40 +2,40 @@
 
 option=$(echo -e "nvim\nspacevim\nzsh\nxresources\ni3\nsxhkd\nmpv\ncustom scripts\npolybar\ndunst\ni3lock\nrofi\nbash\nxinit\nxmonad\nxmobar\nalacritty" | rofi -show drun -i -dmenu --no-custom -p "config")
 
-term=$(which urxvt)
+EDITOR='emacsclient -ncs emacsDaemon'
 
 if [ "$option" == "zsh" ]; then
-  $term -e nvim ~/.zshrc
+  $EDITOR ~/.zshrc
 elif [ "$option" == "i3" ]; then
-  $term -e nvim ~/.config/i3/config
+  $EDITOR ~/.config/i3/config
 elif [ "$option" == "sxhkd" ]; then
-  $term -e nvim ~/.config/sxhkd/sxhkdrc 
+  $EDITOR ~/.config/sxhkd/sxhkdrc 
 elif [ "$option" == "dunst" ]; then
-  $term -e nvim ~/.config/dunst/dunstrc
+  $EDITOR ~/.config/dunst/dunstrc
 elif [ "$option" == "i3lock" ]; then
-  $term -e nvim ~/.config/i3lock/.i3lockrc
+  $EDITOR ~/.config/i3lock/.i3lockrc
 elif [ "$option" == "bash" ]; then
-  $term -e nvim ~/.bashrc
+  $EDITOR ~/.bashrc
 elif [ "$option" == "xinit" ]; then
-  $term -e nvim ~/.xinitrc
+  $EDITOR ~/.xinitrc
 elif [ "$option" == "xmonad" ]; then
-  $term -e nvim ~/.xmonad/xmonad.hs
+  $EDITOR ~/.xmonad/xmonad.hs
 elif [ "$option" == "xmobar" ]; then
-  $term -e nvim ~/.config/xmobar/xmobar.hs
+  $EDITOR ~/.config/xmobar/xmobar.hs
 elif [ "$option" == "alacritty" ]; then
-  $term -e nvim ~/.config/alacritty/alacritty.yml
+  $EDITOR ~/.config/alacritty/alacritty.yml
 elif [ "$option" == "polybar" ]; then
-  $term -e nvim ~/.config/polybar/config
+  $EDITOR ~/.config/polybar/config
 elif [ "$option" == "nvim" ]; then
-  $term -e nvim ~/.config/nvim/init.vim
+  $EDITOR ~/.config/nvim/init.vim
 elif [ "$option" == "xresources" ]; then
-  $term -e nvim ~/.Xresources
+  $EDITOR ~/.Xresources
 elif [ "$option" == "rofi" ]; then
-  $term -e nvim ~/.config/rofi/config.rasi
+  $EDITOR ~/.config/rofi/config.rasi
 elif [ "$option" == "custom scripts" ]; then
   ~/.custom_scripts/scripts_config.sh
 elif [ "$option" == "spacevim" ]; then
-  $term -e nvim ~/.SpaceVim.d/init.toml
+  $EDITOR ~/.SpaceVim.d/init.toml
 elif [ "$option" == "mpv" ]; then
-  $term -e nvim ~/.config/mpv/mpv.conf
+  $EDITOR ~/.config/mpv/mpv.conf
 fi
--- .custom_scripts/scripts_config.sh
+++ .custom_scripts/scripts_config.sh
@@ -3,5 +3,5 @@
 option=$(for file in ~/.custom_scripts/*; do if [ -x "$file" ]; then echo $(basename $file); fi | cut -d. -f1; done | rofi -i -dmenu -p "custom scripts" -no-custom)
 
 if [ -f ~/.custom_scripts/$option.sh ]; then
-  urxvt -e nvim ~/.custom_scripts/$option.sh
+  emacsclient -ncs emacsDaemon ~/.custom_scripts/$option.sh
 fi
